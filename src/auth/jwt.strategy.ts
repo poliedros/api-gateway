5a07db26705cf603db5cf3deadb6b7480db33105
Carlos <carloshlzansavio@gmail.com>
2022-07-03 11:07:12 +0100

    feat: add jwt strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,7 @@
-import { jwtConstants } from './constants';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from './constants';
 import { Payload } from './auth.service';
 
 @Injectable()
