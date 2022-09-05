ecf629f89d552744b4f53ac9974c65e36183eb1a
Carlos <carloshlzansavio@gmail.com>
2022-09-05 13:50:50 +0100

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
@@ -28,6 +29,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('CZAR+ Api Gateway')
+    .setDescription('Api Gateway from CZAR+')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   printRoutes(app);
