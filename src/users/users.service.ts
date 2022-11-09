f52d83f42c70c7b793206e888a2c2f3558d3d1a4
Carlos <carloshlzansavio@gmail.com>
2022-11-09 19:40:11 -0300

    add trycatch block

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,7 +10,11 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async findOne(username: string) {
-    return this.usersRepository.findOne({ username });
+    try {
+      return await this.usersRepository.findOne({ username });
+    } catch (err) {
+      return null;
+    }
   }
 
   async create(createUserRequest: CreateUserRequest) {
